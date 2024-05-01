fc4f34b593e20176edd13640f19ff481eec987ad
rav <ravishk399@gmail.com>
2024-05-01 14:36:04 +0530

    rajasthan

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Home from "./Home"
 import Monument from "./Monument"
+import Rajasthan from "./Rajasthan"
 import Travel from "./Travel"
 
 
@@ -11,6 +12,7 @@ return(
    <Routes>
     <Route exact path='/' element={<Home/>}/>
     <Route exact path='/monument' element={<Monument/>}/>
+    <Route exact path='/rajasthan' element={<Rajasthan/>}/>
     <Route exact path='/travel' element={<Travel/>}/>
    </Routes>
     </>
